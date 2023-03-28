d3f3ec39d9d1b094d6662bb55a0dfbcddf97a483
artemyderik <derry0808@gmail.com>
2023-03-28 20:10:36 +0700

    It Works! Added getQuestion method in Model, set storyboards, set QuestionVC

--- PersonalQuiz/Models/Question.swift
+++ PersonalQuiz/Models/Question.swift
@@ -9,7 +9,6 @@ struct Question {
     let title: String
     let type: ResponseType
     let answers: [Answer]
-    
 }
 
 enum ResponseType {
@@ -32,13 +31,51 @@ enum AnimalType: Character {
     var definition: String {
         switch self {
         case .dog:
-            return "Вам нравится быть с друзьями, вы окружаете себя людьми, которые вам нравятся и всегда готовы помочь"
+            return "Вам нравится быть с друзьями. Вы окружаете себя людьми, которые вам нарвятся и всегда готовы помочь"
         case .cat:
-            return "Вы себе на уме, любите гулять сами по себе, вы цените одиночество"
+            return "Вы себе на уме. Любите гулять сами по себе. Вы цените одиночество"
         case .rabbit:
-            return "Вам нравится все мягкое, вы здоровы и полны энергии"
+            return "Вам нарвится все мягкое. Вы здоровы и полны энерги"
         case .turtle:
             return "Ваша сила - в мудрости. Медленный и вдумчивый выигрывает на больших дистанциях"
         }
+    }
+}
+
+extension Question {
+    
+    static func getQuestions() -> [Question] {
+          [
+            Question(
+                title: "Какую пищу предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", type: .dog),
+                    Answer(title: "Рыба", type: .cat),
+                    Answer(title: "Морковь", type: .rabbit),
+                    Answer(title: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравиться больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", type: .dog),
+                    Answer(title: "Спать", type: .cat),
+                    Answer(title: "Обниматься", type: .rabbit),
+                    Answer(title: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", type: .cat),
+                    Answer(title: "Нервничаю", type: .rabbit),
+                    Answer(title: "Не замечаю", type: .turtle),
+                    Answer(title: "Обожаю", type: .dog)
+                ]
+            ),
+        ]
     }
 }
---
+++ ResultViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Артемий Дериглазов on 25.03.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+//MARK: IB Outlets
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var resultTextLabel: UILabel!
+    
+//MARK: Public Properties
+    var chosenAnswers: [Answer]!
+    
+//MARK: Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+//MARK: IB Actions
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true)
+    }
+}
+
+//MARK: Private Methods
+extension ResultViewController {
+    private func getResult() {
+        var animalsTypes = [AnimalType]()
+        for chosenAnswer in chosenAnswers {
+            animalsTypes.append(chosenAnswer.type)
+        }
+        
+        var animalsWithKeys: [AnimalType : Int] = [:]
+        for animal in animalsTypes {
+            animalsWithKeys[animal] = (animalsWithKeys[animal] ?? 0) + 1
+        }
+        let sortedAnimalsTypes = animalsWithKeys.sorted { $0.value > $1.value }
+        guard let mostFrequencyType = sortedAnimalsTypes.first?.key else { return }
+        setLabels(animal: mostFrequencyType)
+    }
+    
+    private func setLabels(animal: AnimalType) {
+        resultLabel.text = "Вы - \(animal.rawValue)"
+        resultTextLabel.text = "\(animal.definition)"
+    }
+}
+
